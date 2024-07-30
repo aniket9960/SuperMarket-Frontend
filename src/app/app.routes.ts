0fe13a957bf1852d54c80ef4543ad3343c7d40e5
Aniket Jagtap <aniketjagtap9960@gmail.com>
2024-07-31 00:31:59 +0530

    customer order product component templates added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,28 @@ import { RegisterComponent } from './Auth/register/register.component';
 import { LoginComponent } from './Auth/login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { authGuard } from './gaurds/auth.guard';
+import { CustomerComponent } from './dashboard/customer/customer.component';
+import { OrderComponent } from './dashboard/order/order.component';
+import { ProductComponent } from './dashboard/product/product.component';
+import { CustomerListComponent } from './dashboard/customer/customer-list/customer-list.component';
+import { AddCustomerComponent } from './dashboard/customer/add-customer/add-customer.component';
+import { UpdateCustomerComponent } from './dashboard/customer/update-customer/update-customer.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/login', pathMatch: 'full' },
-    { path: '*', redirectTo: '/login' },
+    { path: '', redirectTo: 'login', pathMatch: 'prefix' },
     {path:'register', component:  RegisterComponent},
     {path:'login', component:LoginComponent},
-    {path:'dashboard', component:DashboardComponent, canActivate:[authGuard]}
+    {path:'dashboard', component:DashboardComponent, canActivate:[authGuard], 
+        children: [
+        { path: 'customer', component: CustomerComponent, 
+            children: [
+                {path: '', component: CustomerListComponent},
+                {path:'addCustomer', component: AddCustomerComponent},
+                {path: 'updateCustomer', component: UpdateCustomerComponent}
+            ]},
+        { path: 'order', component: OrderComponent },
+        { path: 'product', component: ProductComponent },
+
+      ]}
     
 ];
